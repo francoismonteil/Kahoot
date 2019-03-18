7c7cf16bea4df3cf05dc096d649bbd1e8f4a803d
francoismonteil <44313306+francoismonteil@users.noreply.github.com>
2019-03-18 11:09:01 +0100

    Start
    Début du projet

---
+++ src/metier/Reponse.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+/**
+ *
+ * @author Black
+ */
+public class Reponse {
+    private int noQuestion;
+    private String texteQuestion;
+    
+}
