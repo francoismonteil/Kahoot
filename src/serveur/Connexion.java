7b39cc2c57de6995dcba5d472038c6a2fe17328f
alexbodart <43186738+alexbodart@users.noreply.github.com>
2019-03-18 11:45:00 +0100

    IHM V2

---
+++ src/serveur/Connexion.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serveur;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author isabelle
+ */
+public class Connexion extends Thread{
+    
+    
+    private Socket socketService;
+    private Serveur monServeur;
+    private int ID  = -1; 
+    private DataInputStream  streamIn  =  null;
+    private DataOutputStream streamOut = null;
+    
+    public Connexion(Socket laSocket,Serveur leServeur)
+    {
+        super();
+        socketService= laSocket;
+        monServeur = leServeur;
+        ID = socketService.getPort();
+        
+    }
+    public void send(String msg)
+    {
+        try
+        {
+            streamOut.writeUTF(msg);
+            streamOut.flush();
+        }
+        catch (IOException ex)
+        {
+            System.out.println(ID + "Erreur à l'envoi du message - " +ex.getMessage());
+            monServeur.remove(ID);
+            interrupt();
+        }
+    }
+   
+     public void open() throws IOException
+    {  
+        streamIn = new DataInputStream(new 
+                        BufferedInputStream(socketService.getInputStream()));
+        streamOut = new DataOutputStream(new
+                        BufferedOutputStream(socketService.getOutputStream()));
+    }
+    public void close() throws IOException
+    {
+        if (socketService != null) socketService.close();
+        if (streamIn != null) streamIn.close();
+        if (streamOut != null) streamOut.close();
+    }
+    
+    public synchronized void envoyerMessage(String msg)
+    {
+        Connexion c;
+        c = monServeur.findConnexion(ID);
+        for (Connexion cc : monServeur.getConnexions())
+        {
+            
+           cc.send(ID + " : " + msg);
+            
+        }
+        if (msg.equals("SALUT"))
+        {
+            c.send(ID + " : "+ msg);
+            monServeur.remove(ID);
+        }
+     
+    }
+    @Override
+    public void run() {
+        
+        while (!Thread.currentThread().isInterrupted())
+        {
+            try 
+            {
+                envoyerMessage(streamIn.readUTF());
+            
+            } catch (IOException ex) 
+            {
+                Logger.getLogger(Connexion.class.getName()).log(Level.SEVERE, null, ex);
+                monServeur.remove(ID);
+                //interrupt();
+            }
+        }
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    
+}
---
+++ src/serveur/EcouteurConnexion.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serveur;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author alexandre
+ */
+public class EcouteurConnexion extends Thread{
+    
+    
+    private ServerSocket socketEcoute = null;
+    protected static List<Connexion> lesConnexionsClient = new ArrayList<Connexion>();
+    protected int nbClients = 0;
+   
+    public EcouteurConnexion() throws IOException
+    {
+        System.out.println("Serveur - Ouverture d'un SocketServer sur le port 50000 ");
+        socketEcoute = new ServerSocket(50000);
+        System.out.println("Serveur démarré : " + socketEcoute);
+        
+    }
+     @Override
+    public void run() {
+        Socket uneSocketClient;
+        Connexion c;
+
+         try {
+                while (!Thread.currentThread().isInterrupted())
+                {
+                    System.out.println("En attente d'un client ");
+                    uneSocketClient = socketEcoute.accept();
+                    ajoutConnexion(uneSocketClient);
+                }            
+            } catch (IOException ex) {
+                Logger.getLogger(Serveur.class.getName()).log(Level.SEVERE, null, ex);
+                Thread.currentThread().interrupt();
+            
+            }
+            finally {
+                try {
+                    System.out.println("Thread serveur interrompu");
+                    socketEcoute.close();
+                    Thread.currentThread().interrupt();
+                } catch (IOException ex) {
+                    Logger.getLogger(Serveur.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+       
+        System.out.println("Thread serveur interrompu");
+    }
+     private void ajoutConnexion(Socket uneSocket)
+    {
+        System.out.println("Client accepté " + uneSocket + " Il y a désormais " + nbClients + " clients connectés");
+        Connexion c = new Connexion(uneSocket,this);
+        try
+        {
+            c.open();
+            c.start();
+            nbClients++;
+            synchronized(lesConnexionsClient){
+            lesConnexionsClient.add(c);
+            }
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Erreur à l'ouverture du thread " + ex);
+        }
+        etatConnexions();
+    }
+     
+    public synchronized void handle(int ID,String msg)
+    {
+        Connexion c;
+        if (msg.equals("SALUT"))
+        {
+           c = findConnexion(ID);
+           c.send("SALUT");
+           remove(ID);
+        }
+        else
+        {
+            for (Connexion cc : lesConnexionsClient)
+            {
+                cc.send(ID + " : " + msg);
+            }
+        }
+    }
+    public Connexion findConnexion(int ID)
+    {
+        Connexion uneConnexion = null;
+        for (Connexion c : lesConnexionsClient)
+        {
+            if (c.getID() == ID)
+            {
+                uneConnexion=c;
+            }
+        }
+        return uneConnexion;
+    }
+   
+    
+    @Override
+    public void interrupt() {
+        super.interrupt(); //To change body of generated methods, choose Tools | Templates.
+    }
+   
+     
+    public synchronized void remove(int ID)
+    {      
+        Connexion uneConnexion = findConnexion(ID);
+        if (uneConnexion != null)
+        {
+            lesConnexionsClient.remove(uneConnexion);
+            nbClients--;
+        }
+        try 
+        {
+            System.out.println("Fermeture du thread d'écoute client " + ID);
+            uneConnexion.close();
+            uneConnexion.interrupt();
+        } catch (IOException ex) 
+        {
+            Logger.getLogger(Serveur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Serveur.remove : Il reste " + nbClients + " clients conectés");
+        etatConnexions();
+
+   }
+    public List<Connexion> getConnexions()
+    {
+        synchronized(lesConnexionsClient) {
+            return lesConnexionsClient;
+        }
+    }
+    public void etatConnexions()
+    {
+        System.out.println("Clients connectés : ");
+        for (Connexion c : lesConnexionsClient)
+        {
+            System.out.println("Connexion : "+ c.getName() + " - " + c.getID() + " Status : " + c.getState());
+        }
+    }
+    public static void main(String[] args) /*throws InterruptedException*/ {
+        
+        EcouteurConnexion monThreadServeur;
+        try {
+            monThreadServeur = new EcouteurConnexion();
+            monThreadServeur.start();
+            
+        } catch (IOException ex) 
+        {
+            Logger.getLogger(Serveur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+                
+    }
+    
+}
